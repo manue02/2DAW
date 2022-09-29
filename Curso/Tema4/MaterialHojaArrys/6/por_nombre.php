ee1abfad08be95b0140d9c8378faae34dade8379
Kimiico <manu2002.03@gmail.com>
2022-09-29 14:49:00 +0200

    ulti

--- Curso/Tema4/MaterialHojaArrys/6/por_nombre.php
+++ Curso/Tema4/MaterialHojaArrys/6/por_nombre.php
@@ -2,14 +2,16 @@
 include("arrays.php");
 
 
-echo "<form action='VerCiudades.php' method='POST'>
-	  <select name='Ciudades'>";
 
-  
+echo "<form action='VerCiudades.php' method='POST'>
+	  <select name='Ciudades'>;
+
+      <option value ='0'> Todos </option>";  
 
 foreach ($arraynombres as $Ciudades => $NombreCiudades) {
 
     echo "<option> ". $NombreCiudades ."</option>";
+ 
     
     }
     echo "</select> <br>
