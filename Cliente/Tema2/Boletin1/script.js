800938b80984159950ff618aa4063e5ad430e3f1
Manuel <manu2002.03@gmail.com>
2022-10-03 03:23:56 +0200

    boletin

---
+++ Cliente/Tema2/Boletin1/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+      //1. Cree un programa que almacene en variables el valor de la edad, el nombre y el
+      //estado civil de una persona y a continuación lo muestre por pantalla.
+
+function Ejercicio1() {
+
+
+
+    let edad = prompt("¿Escribe la edad");
+    let nombre = prompt("¿Escribe el nombre");
+    let estadoCivil = prompt("¿Escribe el Estado Civil");
+
+    console.log(
+      `La edad es: ${edad} , El nombre es: ${nombre} , El estado Civil: ${estadoCivil}`);
+
+}
+
+function Ejercicio2() {
+
+
+
+  let numero = prompt("¿Escribe el sueldo del operario");
+  let nombre = prompt("¿Escribe el nombre");
+
+
+  console.log(
+    `La edad es: ${numero}
+    El nombre es: ${nombre}`);
+
+}
+
+function Ejercicio3() {
+
+
+
+  let edad = prompt("¿Escribe la edad");
+  let nombre = prompt("¿Escribe el nombre");
+  let estadoCivil = prompt("¿Escribe el Estado Civil");
+
+  console.log(
+    `La edad es: ${edad} , El nombre es: ${nombre} , El estado Civil: ${estadoCivil}`);
+
+}
