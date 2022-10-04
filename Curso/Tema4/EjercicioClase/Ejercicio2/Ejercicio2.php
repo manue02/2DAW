1e7d301ce5012315bfcdda465933eed4ccc33424
Kimiico <manu2002.03@gmail.com>
2022-10-04 13:00:39 +0200

    Ejercicios

---
+++ Curso/Tema4/EjercicioClase/Ejercicio2/Ejercicio2.php
@@ -0,0 +1,43 @@
+<?php
+
+include("arrays.php");
+
+$Cursos = array();
+
+echo "<form action='Consultar.php' method='POST'>
+	  <select name='Notas'>
+
+     <table border='1'>
+      <th>Enseñanza</th>
+      <tr>
+
+      <option value ='0'> Todos </option>";  
+
+foreach ($arrayNotas as $Nombres => $Asignatura) {
+	
+
+	foreach ($Asignatura as $NombreAsig => $Notas) {
+
+        if (in_array($NombreAsig, $Cursos) == NULL){
+
+            $Cursos[] = $NombreAsig;
+        }       
+    }
+}
+
+foreach ($Cursos as $Curso) {
+            
+    echo "<option> ". $Curso ."</option>";
+
+}
+
+echo "</select> <br>
+<input type='submit' value='Consultar'>
+
+</tr>
+</table>
+
+</form>";
+
+
+?>
---
+++ Curso/Tema4/EjercicioClase/Ejercicio2/arrays.php
@@ -0,0 +1,31 @@
+<?php
+$arrayNotas = array
+		(
+		'Baja'=>array 
+					('SMR'=>5,
+					 'TECO'=>2),
+		'Suspenso'=>array 
+					('SMR'=>10,
+					 'TECO'=>11,
+					 'DAW'=>6,
+					 'DAM'=>3),
+		  
+		 'Aprobado'=>array 
+					('SMR'=> 6 ,
+					 'TECO'=>8,
+					 'DAW'=>2 ,
+					 'DAM'=>5 ),
+		 'Notable'=>array 
+					('SMR'=> 3 ,
+					 'DAW'=> 2,
+					 'DAM'=> 7),
+		 'Sobresaliente'=>array 
+					('SMR'=> 1 ,
+					 'TECO'=> 2,
+					 'DAM'=> 28),
+		 'Matrícula de honor'=>array 
+					('DAW'=> 1 )			 
+					 
+		);
+
+?>
