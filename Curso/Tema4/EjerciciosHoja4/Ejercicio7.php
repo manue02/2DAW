dea2e3e667c3501de08968a7458024ec4e29ed75
Kimiico <manu2002.03@gmail.com>
2022-10-07 13:39:25 +0200

    ejerrcci

---
+++ Curso/Tema4/EjerciciosHoja4/Ejercicio7.php
@@ -0,0 +1,27 @@
+
+
+<?php
+
+#7. Cree una función que reciba un par de números a y b como argumento y devuelva como resultado un
+#vector asociativo en que en la posición de índice 'suma' se colocará el resultado de la suma de a y b, en la
+#posición de índice 'resta' la diferencia entre el mayor y el menor, en la posición de índice 'producto' la
+#multiplicación y en la de índice 'division' la división el mayor y el menor salvo que el menor sea 0 en cuyo
+#caso devolverá un mensaje.
+#Haga uso de esta función para crear una página web en que se muestren los resultados de las distintas
+#operaciones indicadas para las combinaciones de los número de 5 al 9, tal y como se muestra a continuación: 
+
+function($a , $b){
+
+$Operacion["Suma"]= $a + $b;
+$Operacion["Resta"]= $a - $b;
+$Operacion["Prodcuto"] = $a / $b;
+$Operacion["Multiplicacion"] = $a * $b;
+
+if ($) {
+    
+}
+
+}
+
+
+?>
