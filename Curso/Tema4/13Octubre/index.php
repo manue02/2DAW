01bfeca5e9926faa541db958b963af24905ec3f0
Kimiico <manu2002.03@gmail.com>
2022-10-13 14:17:35 +0200

    terminado

---
+++ Curso/Tema4/13Octubre/index.php
@@ -0,0 +1,24 @@
+<?php
+include("array.php");
+
+
+echo "<form action='VerTabla.php' method='POST'>
+	  <select name='Ciudades'>";
+
+     
+
+foreach ($a as $Ciudades => $Contenido) {
+
+    echo "<option> ". $Ciudades ."</option>";
+ 
+    
+    }
+    echo "</select> <br>
+    <input type='submit' value='Consultar'>
+    </form>";
+
+
+/*echo "<pre>";
+print_r($a);
+echo "<pre>";*/
+?>
