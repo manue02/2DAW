3f43f33259ab50eb24c759eee05fa0d4b03d4d84
Kimiico <manu2002.03@gmail.com>
2022-11-17 14:57:15 +0100

    todo

---
+++ Curso/Tema6/Ejercicio1/ejercicio1Cookies.php
@@ -0,0 +1,22 @@
+<?php
+
+$color = $_POST['radio'];
+
+
+
+
+
+
+if ($color == 'rojo') {
+    print "Rojo";
+
+}
+
+if ($color == 'verde') {
+    print "Es la primera vez que visitas esta página"
+        ;
+} else {
+
+}
+
+?>
