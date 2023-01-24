71196cf4eee3920d93e37439a66567da2c0014fc
Kimiico <manu2002.03@gmail.com>
2023-01-24 11:32:56 +0100

    todo

--- Curso/Examenes/examen_alumnos/ejercicio1/combo.php
+++ Curso/Examenes/examen_alumnos/ejercicio1/combo.php
@@ -1,7 +1,24 @@
 <?php
+include("array1.php");
 
-include(array1 . php)
+echo "<form action='VerDatos.php' method='POST'>
+	  <select name='Datos'>";
+
+
+//mostrar un combo para que muestre los nombres ordenados alfabeticamente y mostrar los datos de la persona elegida
 
 
 
-    ?>
+
+
+echo "</select> <br>";
+
+echo "
+    <input type='submit' value='Ver Idiomas'>
+    </form>";
+
+
+
+
+
+?>
