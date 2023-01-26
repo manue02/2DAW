0d0a08f1f2137778fb0cf260270dd655937664c2
Manuel <manu2002.03@gmail.com>
2023-01-26 18:35:31 +0100

    asdad

--- Curso/Tema3/ScriptTema3/Ejemplo1.php
+++ Curso/Tema3/ScriptTema3/Ejemplo1.php
@@ -1,10 +1,10 @@
-<?php 
-$a=3; 
-$b="3"; 
-if ($a==$b) 
-	print ("A es igual B"); 
-if ($a<$b) 
-	print ("A es menor que B"); 
-print("<br>A no es menor que b, pero esto saldr�<br>");
+<?php
+$a = 3;
+$b = "3";
+if ($a == $b)
+	print("A es igual B");
+if ($a < $b)
+	print("A es menor que B");
+print("<br>A no es menor que b, pero ffooff saldr�<br>");
 
-?> 
+?>
