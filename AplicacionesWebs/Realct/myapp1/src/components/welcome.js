41b834d9ef884216d0b49e6f13d0ecf29267dbc4
Kimiico <manu2002.03@gmail.com>
2023-02-09 11:21:26 +0100

    todo

--- AplicacionesWebs/Realct/myapp1/src/components/welcome.js
+++
@@ -1,7 +0,0 @@
-function welcome(props) {
-	return <h1>Hello, {props.name}</h1>;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const element = <welcome name="Sara" />;
-root.render(element);
