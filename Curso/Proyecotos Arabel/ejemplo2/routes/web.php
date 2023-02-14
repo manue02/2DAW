abfb5fb343a6828eeffab142ecbd339a7fe335aa
Kimiico <manu2002.03@gmail.com>
2023-02-14 12:56:38 +0100

    r

--- Curso/Proyecotos Arabel/ejemplo2/routes/web.php
+++ Curso/Proyecotos Arabel/ejemplo2/routes/web.php
@@ -26,6 +26,3 @@ Route::get('/profesores/ver/{id}', [ProfesorController::class, 'show']);
 Route::get('/profesores/editar/{id}', [ProfesorController::class, 'edit']);
 Route::post('/profesores/editar/{id}', [ProfesorController::class, 'update']);
 Route::get('/profesores/eliminar/{id}', [ProfesorController::class, 'destroy']);
-Route::get ('pruebaswhere ', function(){
-    $curso
-});
