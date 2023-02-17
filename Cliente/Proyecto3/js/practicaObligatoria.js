e0761654c7a5c5196a009641ce4490b63273a16e
Kimiico <manu2002.03@gmail.com>
2023-02-17 14:48:30 +0100

    todo

--- Cliente/Proyecto3/js/practicaObligatoria.js
+++ Cliente/Proyecto3/js/practicaObligatoria.js
@@ -6,25 +6,6 @@ let Gestores = new Array(9);
 let contador = 0;
 const apiRest = "https://primerproyecto-34e1f-default-rtdb.asia-southeast1.firebasedatabase.app/";
 
-// categorias = ["Bebidas", "Tostadas", "Bollería"];
-
-// catalogo.addProducto(1, "Café con leche", 0.95, 0);
-// catalogo.addProducto(2, "Té", 1.05, 0);
-// catalogo.addProducto(3, "Cola-cao", 1.35, 0);
-// catalogo.addProducto(4, "Chocolate a la taza", 1.95, 0);
-// catalogo.addProducto(5, "Media con aceite", 1.25, 1);
-// catalogo.addProducto(6, "Entera con aceite", 1.95, 1);
-// catalogo.addProducto(7, "Media con aceite y jamón", 1.95, 1);
-// catalogo.addProducto(8, "Entera con aceite y jamón", 2.85, 1);
-// catalogo.addProducto(9, "Media con mantequilla", 1.15, 1);
-// catalogo.addProducto(10, "Entera con mantequilla", 1.75, 1);
-// catalogo.addProducto(11, "Media con manteca colorá", 1.45, 1);
-// catalogo.addProducto(12, "Entera con manteca colorá", 2.15, 1);
-// catalogo.addProducto(13, "Croissant", 0.95, 2);
-// catalogo.addProducto(14, "Napolitana de chocolate", 1.45, 2);
-// catalogo.addProducto(15, "Caracola de crema", 1.65, 2);
-// catalogo.addProducto(16, "Caña de chocolate", 1.35, 2);
-
 frmControles.categorias.addEventListener("change", CategoriaSeleccionada);
 
 document.addEventListener("DOMContentLoaded", colorearMesasLibres);
@@ -48,10 +29,13 @@ function MostrarCategorias(listaCategorias) {
 
 	const frmControles = document.getElementById("frmControles");
 
+	const frmNuevoProducto = document.getElementById("frmNuevoProducto");
+
 	for (let categoria of listaCategorias) {
 		let lista = document.createElement("option");
 		lista.innerHTML = categoria;
 		frmControles.categorias.add(lista);
+		frmNuevoProducto.categoria.add(lista.cloneNode(true));
 	}
 }
 
@@ -84,6 +68,8 @@ function CategoriaSeleccionada(listaProductos) {
 		frmControles.productos.add(option);
 		console.log(producto);
 	});
+
+	//console.log(categoria);
 }
 
 //colorear todas las mesas libres
